7fd08cdd50fb723b31b8a6af9a0f18d9d2d72d58
FND <fnd@fnd-oda.(none)>
2010-11-14 10:19:13 +0100

    fixed PEP8 issues

--- presenterm.py
+++ presenterm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-import os, curses
+import os
+import curses
+
 
 class PresenTerm:
 
@@ -10,8 +12,8 @@ class PresenTerm:
 		self.offsety, self.offsetx = -self.height / 2, -self.width / 2
 		self.cur = 0
 		self.slides = []
-		self.dirname = '.' # todo: from arg
-	
+		self.dirname = '.'  # TODO: from arg
+
 	def readSlides(self):
 		for dirname, dirnames, filenames in os.walk(self.dirname):
 			for filename in filenames:
@@ -19,39 +21,45 @@ class PresenTerm:
 					with open(filename) as f:
 						self.slides.append(f.read())
 					f.close()
-						
+
 	def showCounter(self):
 		if self.cur < 1:
 			self.cur = 1
 		if self.cur > len(self.slides):
 			self.cur = len(self.slides)
-		self.screen.addstr(0, 0, 'presenterm: %(dir)s: %(cur)s of %(count)s' % \
-			{ 'dir': self.dirname, 'cur': self.cur, 'count': len(self.slides) }, \
+		self.screen.addstr(0, 0, 'presenterm: %(dir)s: %(cur)s of %(count)s' %
+			{ 'dir': self.dirname, 'cur': self.cur, 'count': len(self.slides) },
 			curses.A_REVERSE)
-			
+
 	def showSlide(self):
-		self.screen.addstr(3, 0, '%(slide)s' % { 'slide': self.slides[self.cur - 1] }, curses.A_NORMAL)
-		
+		self.screen.addstr(3, 0,
+			'%(slide)s' % { 'slide': self.slides[self.cur - 1] },
+			curses.A_NORMAL)
+
 	def moveSlide(self, moveTo):
 		self.cur = self.cur + moveTo
 		self.refresh()
-			
+
 	def refresh(self):
 		self.screen.clear()
 		self.showCounter()
 		self.showSlide()
-	
+
 	def main(self):
 		self.readSlides()
 		self.refresh()
 		while 1:
 			key = self.screen.getch()
-			if key == ord('q'): break
-			elif key == curses.KEY_LEFT or key == ord('a'): self.moveSlide(-1)
-			elif key == curses.KEY_RIGHT or key == ord('d'): self.moveSlide(1)
+			if key == ord('q'):
+				break
+			elif key == curses.KEY_LEFT or key == ord('a'):
+				self.moveSlide(-1)
+			elif key == curses.KEY_RIGHT or key == ord('d'):
+				self.moveSlide(1)
+
 
 def main(stdscr):
 	screen = PresenTerm(stdscr)
 	screen.main()
- 
+
 curses.wrapper(main)
